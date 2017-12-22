a67fbf3d44f5eb1b78a78d74833b1557a60f9b5e
developer <developer@vadyms-MacBook-Air.local>
2017-12-22 15:30:18 +0200

    start rhyme project commit

---
+++ src/main/java/com/ua/rhyme/RhymeApplication.java
@@ -0,0 +1,12 @@
+package com.ua.rhyme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhymeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhymeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ua/rhyme/RhymeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ua.rhyme;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RhymeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
